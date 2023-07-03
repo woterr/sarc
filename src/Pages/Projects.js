151be076070eb6505ea5361ebc957403c5175058
woterr <83902185+woterr@users.noreply.github.com>
2023-07-04 00:38:37 +0530

    Update Projects.js

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -43,14 +43,44 @@ function Projects() {
             data-aos-duration="800"
             data-aos-delay="200"
           >
-            {projectArray.map((item, i) => (
-              <a href={item.link} className="item" key={i}>
+            // {projectArray.map((item, i) => (
+            //   <a href={item.link} className="item" key={i}>
+            //     <div className="item__data">
+            //       <h3 className="item-title">{item.title}</h3>
+            //       <div>{item.description}</div>
+            //     </div>
+            //   </a>
+            // ))}
+              <a href="https://stickmansaga.com/" className="item">
                 <div className="item__data">
-                  <h3 className="item-title">{item.title}</h3>
-                  <div>{item.description}</div>
+                  <h3 className="item-title">Stickman Saga</h3>
+                  <div>PvP P2E battle arena, battle and compete to win $STIX and rewards in the ecosystem.</div>
                 </div>
               </a>
-            ))}
+              <a href="https://www.jazzheroes.io/" className="item">
+                <div className="item__data">
+                  <h3 className="item-title">Jazz Heroes</h3>
+                  <div>The first ever player’s initiative NFT collection that illustrates Utah Jazz players as superheroes. By the players for the Community!</div>
+                </div>
+              </a>
+              <a href="https://y0nft.com/" className="item">
+                <div className="item__data">
+                  <h3 className="item-title">Y0</h3>
+                  <div>Bridging The Gap Between Science Fiction & Reality.</div>
+                </div>
+              </a>
+              <a href="https://www.graveyarddao.com/" className="item">
+                <div className="item__data">
+                  <h3 className="item-title">Graveyard DAO</h3>
+                  <div>The Graveyard DAO is a community of NFT collectors who are tired of holding onto dead NFT collections.</div>
+                </div>
+              </a>
+              <a href="https://discord.gg/notion" className="item">
+                <div className="item__data">
+                  <h3 className="item-title">Notion Community</h3>
+                  <div>This community-run Discord server is all about Notion, the future of productivity apps.</div>
+                </div>
+              </a>
           </div>
         </div>
       </section>
