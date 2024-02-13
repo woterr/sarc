61d030d1d9f5bc618a20765c7ec32caeafecb631
woterr <shreyasv320@gmail.com>
2024-02-13 21:15:39 +0530

    update logo

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useEffect, useState } from "react";
+import twitch_logo from "../Images/twitch.svg";
 
 function Home() {
   const time = new Date().toLocaleTimeString("en-US", {
@@ -97,18 +98,7 @@ function Home() {
               <i className="bx bxl-instagram-alt"></i>
             </a>
             <a href=" https://www.twitch.tv/raaj">
-              <svg
-                fill="#ffffff"
-                className="svgicon"
-                viewBox="0 0 256 256"
-                id="Flat"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g id="SVGRepo_iconCarrier">
-                  {" "}
-                  <path d="M208,32H48A16.01833,16.01833,0,0,0,32,48V192a16.01833,16.01833,0,0,0,16,16H64v32a7.99949,7.99949,0,0,0,13.12109,6.14551L122.89648,208h42.207a16.03033,16.03033,0,0,0,10.24316-3.709l42.896-35.7461A15.958,15.958,0,0,0,224,156.253V48A16.01833,16.01833,0,0,0,208,32ZM128,136a8,8,0,0,1-16,0V88a8,8,0,0,1,16,0Zm48,0a8,8,0,0,1-16,0V88a8,8,0,0,1,16,0Z"></path>{" "}
-                </g>
-              </svg>
+              <img src={twitch_logo} className="twitch_icon" />
             </a>
           </div>
         </div>
